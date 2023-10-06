15377d8b332f52c020a449bc53a66512b1b0672d
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-06 23:28:36 +0000

    Numbers and calculations of math

---
+++ 1_BasicJS.js
@@ -0,0 +1,4 @@
+// Giving instructions to a computer : Code
+//Computer follows the instructions : running the code
+
+//In programming we have to follow the syntax exactly unlike the english language, otherwise computer wont understand the code
---
+++ 2_Num&Math.js
@@ -0,0 +1,4 @@
+// JS follows the same order of opeartions as maths
+// * && / have same priority and + & - have same...It calculates from left to right
+
+//W can use (....) to make some calculations to be done first in side the bracket
