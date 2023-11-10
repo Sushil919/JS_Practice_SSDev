4187bd0460d269738f34bc53ab1a6cad8754b7cf
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-11-10 23:07:25 +0000

    exercise 1

---
+++ JavaScript Basics/Exercise_1/1f-j.js
@@ -0,0 +1,11 @@
+document.body.innerHTML = 'Good Morning!';
+
+document.body.innerHTML = 'Sushil';
+
+10 + 8 + 20
+
+100 - 20 - 50 + 200
+
+//make the web page blank
+
+document.body.innerHTML = '';
