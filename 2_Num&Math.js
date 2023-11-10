293f72c9d0dd3abd48644870e86bcc6d595215c3
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-11-10 23:18:50 +0000

    brackets

--- 2_Num&Math.js
+++ 2_Num&Math.js
@@ -1,4 +1,10 @@
 // JS follows the same order of opeartions as maths
 // * && / have same priority and + & - have same...It calculates from left to right
 
-//W can use (....) to make some calculations to be done first in side the bracket
+
+//calculate 10%
+
+// 36.93 * 0.1 (10/100)
+// 3.693
+
+//We can use (....) to make some calculations to be done first in side the bracket
